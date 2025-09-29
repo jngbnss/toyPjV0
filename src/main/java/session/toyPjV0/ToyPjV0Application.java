c4580e46b914c42349f14125e3fe13c9644e004b
jngbnss <jngbnmn@naver.com>
2025-09-29 11:02:14 +0900

    Initial commit from IntelliJ

---
+++ src/main/java/session/toyPjV0/ToyPjV0Application.java
@@ -0,0 +1,13 @@
+package session.toyPjV0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyPjV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyPjV0Application.class, args);
+	}
+
+}
---
+++ src/test/java/session/toyPjV0/ToyPjV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package session.toyPjV0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyPjV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
